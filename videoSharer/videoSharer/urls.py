0f4f69756bd12d739616e0b0b4b5f16b96abe329
TecnaGamer <34553765+TecnaGamer@users.noreply.github.com>
2021-03-01 14:17:59 -0500

    indx

--- videoSharer/videoSharer/urls.py
+++ videoSharer/videoSharer/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from videos import views as video_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', video_views.index, name='index'),
 ]
